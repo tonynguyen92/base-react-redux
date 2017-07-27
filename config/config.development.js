3a5958d6d15d3e3924314221dbe43d945d05d80e
NDK304 <ndk3004@yahoo.com>
2017-07-27 12:13:30 +0700

    environment runtime

---
+++ config/config.development.js
@@ -0,0 +1,7 @@
+// Here is where you can define configuration overrides based on the execution environment.
+// Supply a key to the default export matching the NODE_ENV that you wish to target, and
+// the base configuration will apply your overrides before exporting itself.
+module.exports = {
+  // compiler_public_path : 'http://${config.server_host}:${config.server_port}/'
+  api_domain: 'https://oct-api-staging.inspicorp.com',
+}
